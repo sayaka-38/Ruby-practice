5c3d8673a42a0f3bc3471762ed713d2a2e01e4e2
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-17 17:19:40 +0900

    Add Monster

--- main.rb
+++ main.rb
@@ -1,3 +1,41 @@
 class Brave
 
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+
 end
+
+class Monster
+
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+
+end
+
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+
+puts <<~TEXT
+NAME:#{brave.name}
+HP:#{brave.hp}
+OFFENSE:#{brave.offense}
+DEFENSE:#{brave.defense}
+TEXT
+
+brave.hp -= 30
+
+puts "#{brave.name}はダメージを受けた！　残りHPは#{brave.hp}だ"
