94799eaf4aa1580cad5d1ebceaa1de21aa358227
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-17 16:49:12 +0900

    Add get param

--- main.rb
+++ main.rb
@@ -4,16 +4,32 @@ class Brave
     @name = name
   end
 
+  def name
+    @name
+  end
+
   def hp=(hp)
     @hp = hp
+  end
+
+  def hp
+    @hp
   end
 
   def offense=(offense)
     @offense = offense
   end
 
+  def offense
+    @offense
+  end
+
   def defense=(defense)
     @defense = defense
+  end
+
+  def defense
+    @defense
   end
 
 end
@@ -24,3 +40,10 @@ brave.name = "テリー"
 brave.hp = 500
 brave.offense = 150
 brave.defense = 100
+
+puts <<~TEXT
+NAME:#{brave.name}
+HP:#{brave.hp}
+OFFENSE:#{brave.offense}
+DEFENSE:#{brave.defense}
+TEXT
