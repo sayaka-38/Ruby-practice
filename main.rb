1cb0d9f94ba914c85b4918d3975057a5cfd674fa
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-17 16:32:49 +0900

    Add Brave-parameters

--- main.rb
+++ main.rb
@@ -1,3 +1,26 @@
 class Brave
 
+  def name=(name)
+    @name = name
+  end
+
+  def hp=(hp)
+    @hp = hp
+  end
+
+  def offense=(offense)
+    @offense = offense
+  end
+
+  def defense=(defense)
+    @defense = defense
+  end
+
 end
+
+brave = Brave.new
+
+brave.name = "テリー"
+brave.hp = 500
+brave.offense = 150
+brave.defense = 100
