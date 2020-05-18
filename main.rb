fe068d5717f579c62e0b40e86b3832a310470b79
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-18 23:52:13 +0900

    Add monsATK && bravATK

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ class Brave
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
@@ -10,18 +12,72 @@ class Brave
     @defense = params[:defense]
   end
 
+  def attack(monster)
+    puts "#{name}の攻撃！"
+    
+    attack_num = rand(4)
+    if attack_num == 0
+      puts "急所に当たった！"
+      damege = calculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damege = @offense - monster.defense
+    end
+    
+    monster.hp -= damege
+
+    puts "#{monster.name}は#{damege}のダメージを受けた"
+    puts "#{monster.name}の残り体力は#{monster.hp}だ"
+  end
+
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
+  end
+
 end
 
 class Monster
 
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  attr_reader :offense, :defense
+  attr_accessor :hp, :name
+
+  PAWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
 
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
+  end
+
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+    puts "#{@name}の攻撃！"
+
+    damege = @offense - brave.defense
+    brave.hp -= damege
+
+    puts "#{brave.name}は#{damege}のダメージを受けた！"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+  def transform
+    transform_name = "ドラゴン"
+    puts <<~EOS
+    #{@name}は怒っている！
+    #{@name}はドラゴンに変身した！
+    EOS
+    @offense *= PAWER_UP_RATE
+    @name = transform_name
   end
 
 end
@@ -29,13 +85,5 @@ end
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-puts <<~TEXT
-NAME:#{brave.name}
-HP:#{brave.hp}
-OFFENSE:#{brave.offense}
-DEFENSE:#{brave.defense}
-TEXT
-
-brave.hp -= 30
-
-puts "#{brave.name}はダメージを受けた！　残りHPは#{brave.hp}だ"
+brave.attack(monster)
+monster.attack(brave)
